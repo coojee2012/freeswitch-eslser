d985f668208f6dca855abe01ee1961304ad49623
林勇 <liny@unicall.cc>
2016-09-26 14:53:19 +0800

    fix bug

---
+++ src/decls/test.js
@@ -0,0 +1,10 @@
+/* @flow */
+/**
+ * Created by linyong on 9/23/16.
+ */
+declare function isLeapYear(year: string): bool;
+
+declare class Counter {
+val: number;
+incr(): void;
+}
--- tests/flowtest.js
+++ tests/flowtest.js
@@ -1,13 +1,13 @@
 /* @flow */
 /**
  * Created by linyong on 9/23/16.
+ *  @flow
  */
 "use strict";
 require('babel-core/register');
 require('babel-polyfill');
+const isLeapYear = (year:string) => {
+ return 'ddddd'
+}
 
-var free = require('../src/adaptors/freeswitch/index').default;
-
-free.answer().then(str =>{
-  console.log(str);
-});
+console.log(isLeapYear('dddd'));
